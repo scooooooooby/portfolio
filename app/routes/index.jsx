af32f3296e520c9e36ec65fced3028c049f8798c
Helen V. Holmes <helen.v.holmes@gmail.com>
2022-03-02 15:23:48 -0500

    Displays some stuff below badly

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -4,7 +4,8 @@ import {
   useLoaderData
 } from "remix";
 import { NotionAPI } from "notion-client";
-import { NotionRenderer } from "react-notion-x";
+import { getPageTitle, getAllPagesInSpace } from 'notion-utils';
+import { Collection, CollectionRow, NotionRenderer } from "react-notion-x";
 
 export async function loader() {
   const blockId = "eed8fd8b18414fd385b68e800822d198";
@@ -19,7 +20,43 @@ export default function Index() {
   const data = useLoaderData();
   return (
     <div>
-      <NotionRenderer recordMap={data} fullPage={true} darkMode={false} />
+      <NotionRenderer 
+        recordMap={data} 
+        fullPage={true} 
+        darkMode={false} 
+        customImages={true} 
+        rootDomain='localhost:3000' // used to detect root domain links and open this in the same tab
+        components={{
+          image: ({
+            src,
+            alt,
+
+            height,
+            width,
+
+            className,
+            style,
+            loading,
+            decoding,
+            
+            ref,
+            onLoad
+          }) => (
+            <img
+            className={className}
+            style={style}
+            src={src}
+            ref={ref}
+            width={width}
+            height={height}
+            loading='lazy'
+            alt={alt}
+            decoding='async'
+          />
+          ),
+          collection: Collection,
+          collectionRow: CollectionRow
+        }} />
       <Link to="/wicching">Wicching Hour</Link>
     </div>
   );
